27e1313b5f414454d9f825c5ee35b683f9a22d5b
Mick <mickx.shular@gmail.com>
2022-01-28 23:54:44 -0500

    more UI added

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using TMPro;
 public class GameController : MonoBehaviour
 {
     public bool scanMode;
     public int amountOfScans;
     public int amountOfExcavations;
     public int amountOfPoints;
+    public Button scanButton;
+    public Button exavationButton;
+    public TextMeshProUGUI numberOfScansGUI;
+    public TextMeshProUGUI numberOfExcavationsGUI;
+    public TextMeshProUGUI amountOfOreCollectedGUI;
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -24,21 +32,32 @@ public class GameController : MonoBehaviour
     public void setAmountOfPoints(int amount)
     {
         amountOfPoints += amount;
+        amountOfOreCollectedGUI.text = "Amount Of Ore Collected: " + amountOfPoints.ToString();
     }
 
     public void setAmountOfScans(int amount)
     {
         amountOfScans += amount;
+        numberOfScansGUI.text = amountOfScans.ToString();
     }
 
     public void setAmountOfExcavations(int amount)
     {
         amountOfExcavations += amount;
+        numberOfExcavationsGUI.text = amountOfExcavations.ToString();
     }
 
-    public void toggleScanAndExcavation()
+    public void ScanButton()
     {
+        scanButton.interactable = false;
+        exavationButton.interactable = true;
         scanMode = !scanMode;
     }
 
+    public void ExcavationButton()
+    {
+        scanButton.interactable = true;
+        exavationButton.interactable = false;
+        scanMode = !scanMode;
+    }
 }
