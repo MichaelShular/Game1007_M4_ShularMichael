ef207b721ae13f0b9822f410d7ef577bcd191426
Mick <mickx.shular@gmail.com>
2022-03-25 22:11:50 -0400

    grid slots, falling in place, check with will be destroyed

--- Assets/Scripts/MatchThree/MatchItemMovementController.cs
+++ Assets/Scripts/MatchThree/MatchItemMovementController.cs
@@ -14,12 +14,15 @@ public class MatchItemMovementController : MonoBehaviour
     public float _speed;
     private Vector3 _direction;
     private float _YStoppingPosition;
+    public float _YStoppingPositionMax;
+    public Transform check;
     public MatchItemStates _currentState;
 
     // Start is called before the first frame update
     void Start()
     {
         _direction = Vector3.down;
+        //checkBelow();
     }
 
     // Update is called once per frame
@@ -57,8 +60,21 @@ public class MatchItemMovementController : MonoBehaviour
     public void setYStoppingPosition(float number)
     {
         _YStoppingPosition = number;
+        
     }
+    //public void checkBelow()
+    //{
+    //    Debug.Log("3f");
+    //    if (_currentState != MatchItemStates.Stopped) return;
+    //    Debug.Log("ff");
+    //    RaycastHit2D hit = Physics2D.Raycast(check.position, Vector2.down, 100.0f);
+    //    if (hit.collider == null)
+    //    {
+    //        Debug.Log("aa");
+    //    }
+    //}
 
+    
 
 
 }
--- Assets/Scripts/MatchThree/MatchThreeGameController.cs
+++ Assets/Scripts/MatchThree/MatchThreeGameController.cs
@@ -7,19 +7,42 @@ public class MatchThreeGameController : MonoBehaviour
     public int _gridSize;
     //public int gridYSize;
     public GameObject _MatchThreeObject;
+    public GameObject _slot;
+
     private GameObject[,] cells;
+    private List<GameObject> matchingList;
 
     // Start is called before the first frame update
     void Start()
     {
         cells = new GameObject[_gridSize, _gridSize];
+        matchingList = new List<GameObject>();
         creatingGameBoard();
+        createGrid();
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    private void createGrid()
+    {
+        float boardSize = _gridSize * 20 + (_gridSize - 1 * 2);
+        float bottomLimit = 0 - boardSize / 2;
+
+        for (int i = 0; i < _gridSize; i++)
+        {
+            for (int j = 0; j < _gridSize; j++)
+            {
+
+                GameObject temp = Instantiate(_slot, this.transform);
+                cells[i, j] = temp;
+                temp.transform.localPosition = new Vector3(bottomLimit + 22 * j, -50 + 22 * i, 0);
+                
+            }
+        }
     }
 
     private void creatingGameBoard()
@@ -36,7 +59,7 @@ public class MatchThreeGameController : MonoBehaviour
                 GameObject temp = Instantiate(_MatchThreeObject, this.transform);
                 cells[i, j] = temp;
                 temp.transform.localPosition = new Vector3(bottomLimit + 22 * i, 100 + 22 * j, 0);
-                temp.GetComponent<MatchItemMovementController>().setYStoppingPosition(bottomLimit + 22 * j);
+                temp.GetComponent<MatchItemMovementController>().setYStoppingPosition(-500);
                 temp.GetComponent<MatchItemMovementController>()._currentState = MatchItemStates.Falling;
                 temp.GetComponent<MatchItemColor>().RandomColor();
                 temp.GetComponent<MatchItemColor>().num = count;
@@ -44,7 +67,7 @@ public class MatchThreeGameController : MonoBehaviour
 
             }
         }
-        checkBoard();
+        //checkBoard();
     }
 
     private void checkBoard()
@@ -57,24 +80,53 @@ public class MatchThreeGameController : MonoBehaviour
             count = 0;
             for (int j = 0; j < _gridSize; j++)
             {
+                //Debug.Log(count);
+               
                 if(countColorHor == cells[i, j].GetComponent<MatchItemColor>()._currentColor)
                 {
                     count++;
                     
-                    if(count >= 2)
+                    if (count >= 2)
                     {
-                        cells[i, j].GetComponent<MatchItemColor>()._image.color = Color.black;
-                        Debug.Log("Hit");
+                        for (int k = 0; k < matchingList.Count; k++)
+                        {
+                            matchingList[k].transform.localScale = Vector3.one * 0.2f;
+                            //Destroy(matchingList[k].gameObject);
+                        }
+                        //cells[i, j].GetComponent<MatchItemColor>()._image.color = Color.black;
+                        //Debug.Log("Hit");
                     }
                 }
                 else
                 {
+                    if (count >= 2)
+                    {
+                        for (int k = 0; k < matchingList.Count; k++)
+                        {
+                            matchingList[k].transform.localScale = Vector3.one * 0.2f;
+                            //Destroy(matchingList[k].gameObject);
+                        }
+                    }
                     count = 0;
+                   
+                    matchingList.Clear();
+                    
+                    
                 }
                 countColorHor = cells[i, j].GetComponent<MatchItemColor>()._currentColor;
-     
-                Debug.Log(count + " "+ countColorHor);
-                
+                matchingList.Add(cells[i, j]);
+                //Debug.Log(matchingList);
+
+            }
+            if (count >= 2)
+            {
+                for (int k = 0; k < matchingList.Count; k++)
+                {
+                    matchingList[k].transform.localScale = Vector3.one * 0.2f;
+                    //Destroy(matchingList[k].gameObject);
+                }
+                //cells[i, j].GetComponent<MatchItemColor>()._image.color = Color.black;
+                //Debug.Log("Hit");
             }
         }
 
@@ -84,26 +136,57 @@ public class MatchThreeGameController : MonoBehaviour
             count = 0;
             for (int i = 0; i < _gridSize; i++)
             {
+               
                 if (countColorHor == cells[i, j].GetComponent<MatchItemColor>()._currentColor)
                 {
                     count++;
+                    
 
                     if (count >= 2)
                     {
-                        cells[i, j].GetComponent<MatchItemColor>()._image.color = Color.black;
-                        Debug.Log("Hit");
+                        for (int k = 0; k < matchingList.Count; k++)
+                        {
+                            matchingList[k].transform.localScale = Vector3.one * 0.2f;
+                            //Destroy(matchingList[k].gameObject);
+                        }
+                        //cells[i, j].GetComponent<MatchItemColor>()._image.color = Color.black;
+                        //Debug.Log("Hit");
                     }
                 }
                 else
                 {
+                    if (count >= 2)
+                    {
+                        for (int k = 0; k < matchingList.Count; k++)
+                        {
+                            matchingList[k].transform.localScale = Vector3.one * 0.2f;
+                            //Destroy(matchingList[k].gameObject);
+                        }
+                    }
                     count = 0;
+                    
+                    matchingList.Clear();
+                    
                 }
                 countColorHor = cells[i, j].GetComponent<MatchItemColor>()._currentColor;
-
-                Debug.Log(count + " " + countColorHor);
+                 matchingList.Add(cells[i, j]);
+                //Debug.Log(count + " " + countColorHor);
 
             }
+            if (count >= 2)
+            {
+                for (int k = 0; k < matchingList.Count; k++)
+                {
+                    matchingList[k].transform.localScale = Vector3.one * 0.2f;
+                    //Destroy(matchingList[k].gameObject);
+                }
+                //cells[i, j].GetComponent<MatchItemColor>()._image.color = Color.black;
+                //Debug.Log("Hit");
+            }
         }
+
+        
+
     }
 
 }
