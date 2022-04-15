f37cf2f192c9157d1725cb50e7c806cf184d53fd
Mick <mickx.shular@gmail.com>
2022-04-15 15:14:32 -0400

    Updates UI and added no function hacking skill level

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -9,10 +9,15 @@ public class PlayerStats : MonoBehaviour
     public TextMeshProUGUI _UISkillLevel;
     public int _numberOfPicks;
     public TextMeshProUGUI _UINumberOfPicks;
+    public int _HackingSkillLevel;
+    public TextMeshProUGUI _HackingLevelUI;
+
     void Start()
     {
         _UISkillLevel.text = "Lockpicking Skill Level: " + _LockSkillLevel.ToString();
         _UINumberOfPicks.text = "Number of Picks: " + _numberOfPicks;
+        _HackingLevelUI.text = "Hacking Skill Level: " + _HackingSkillLevel.ToString();
+
     }
     public void changeSkillLevel()
     {
@@ -22,6 +27,15 @@ public class PlayerStats : MonoBehaviour
             _LockSkillLevel = 0;
         }
         _UISkillLevel.text = "Lockpicking Skill Level: " + _LockSkillLevel.ToString();
+    }
 
+    public void changeHackingSkillLevel()
+    {
+        _HackingSkillLevel++;
+        if (_HackingSkillLevel > 10)
+        {
+            _HackingSkillLevel = 0;
+        }
+        _HackingLevelUI.text = "Hacking Skill Level: " + _HackingSkillLevel.ToString();
     }
 }
